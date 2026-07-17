e9d3e5e5a740518e28320d40ccc7c23d87c74d4b
Sanjeev bro <your@email.com>
2026-07-17 14:50:12 +0530

    Added Part 2 - Reading & Writing to Console

---
+++ Part2/Part2/Part2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Reading & Writing to Console
+/*
+ * In this session 
+
+    Reading from the console
+    Writing to the console
+    3 ways to write to console
+     a) Concatenation
+     b) Place holder syntax - Most prefreed
+     c) Interpolation - Most prefered
+*/
+
+/*
+namespace Part2
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your name");
+
+           string UserName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {UserName}");
+
+            //Console.WriteLine("Hello {0}" + UserName);
+
+            //Console.WriteLine("Hello " + UserName);
+        }
+    }
+}
+
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your first name");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name");
+        string LastName = Console.ReadLine();
+
+        //Console.WriteLine("Hello " + FirstName + " " + LastName);
+
+        //Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+
+        Console.WriteLine($"Hello {FirstName} {LastName}");
+    }
+}
+
+//Note: C# is case sensitive
